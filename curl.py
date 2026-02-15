a873bbe9f6f3c804d55364a5a157bd021c1d190e
Mary E <nemuri.books@gmail.com>
2026-02-15 10:49:57 +0300

    Sprint 5 tests selenium not curl

--- curl.py
+++
@@ -1,7 +0,0 @@
-main_site = 'https://stellarburgers.education-services.ru'
-
-name=f'{main_site}/register'
-email=f'{main_site}/login'
-password=f'{main_site}/login'
-
-password_recovery=f'{main_site}/forgot-password'
